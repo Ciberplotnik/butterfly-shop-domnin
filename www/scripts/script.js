226c574896d76069a21ed805a56a0cd8cb59fcfe
Vitaly <domnin.vitaly2024@gmail.com>
2024-07-06 16:21:29 +0300

    сделал аккордион, пока без иконки

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,98 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  let prevAccordionBtn;
+
+  $('.js-accordion-btn').on('click', function () {
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevAccordionBtn = this;
+  })
+
+  let aaa = $('.accordion_content');
+
+  console.log(aaa);
+
+  function onAnyChange(){
+    if ($('.js-accordion-btn').next().css('display') != 'none'){
+    $(this).children('accordion_icon').css({
+      'background-image': 'url(../images/accordion-icon-minus.svg)',
+    })
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });
+
