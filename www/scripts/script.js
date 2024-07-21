96e5633c9dd540aa86d4a77ddc9207f4c6647b37
Vitaly <domnin.vitaly2024@gmail.com>
2024-07-21 13:16:04 +0300

    make clide header-menu

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -8,24 +8,13 @@ $(document).ready(function() {
       $(this).next().slideToggle();
       return;
     }
-
-
-
-
     // $('.js-accordion-icon').addClass('toggle_icon-plus');
     $(this).children('.js-accordion-icon').toggleClass('toggle_icon-plus toggle_icon-minus')
-
 
     $('.js-accordion-btn').next().slideUp();
     $(this).next().slideDown();
     prevAccordionBtn = this;
-
   })
-
-
-
-
-
 
 
   $('.js-tabs-link-city').on('click', function (event) {
@@ -40,9 +29,35 @@ $(document).ready(function() {
     $('.js-contacts-card').eq(index).addClass('active');
   })
 
+  $('.js-portfolio-tag-link').on('click', function (event) {
+    event.preventDefault();
+
+    let dataFilter = $(this).data('filter');
+
+    if (dataFilter === 'all') {
+      $('.js-portfolio-list > li').show();
+      return;
+    }
+
+    $('.js-portfolio-list > li').each(function () {
+      let dataType = $(this).data('type');
+
+      if (dataFilter === dataType) {
+        $(this).show();
+        return;
+      }
+      $(this).hide();
+    })
+
+  })
+
+  $('.burger').on('click', function (event) {
+    event.preventDefault();
+
+    $('.js-main-nav').slideToggle();
 
 
-
+  })
 
 
 
